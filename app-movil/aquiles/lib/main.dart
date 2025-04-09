652788e4b7e378033dd95e2ec75277d928bae78a
Juan Montilla <Juanidequilmes@gmail.com>
2025-04-09 15:26:35 -0300

    style: cambio ui de login

--- app-movil/aquiles/lib/main.dart
+++ app-movil/aquiles/lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:aquiles/screens/activity_tracking_screen.dart';
-import 'package:aquiles/screens/device_control_screen.dart';
-import 'package:aquiles/screens/home_screen.dart';
-import 'package:aquiles/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'screens/login_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const AquilesApp());
 }
 
@@ -14,14 +17,33 @@ class AquilesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Aquiles App',
       debugShowCheckedModeBanner: false,
-      title: 'Aquiles',
-      routes: {
-        '/': (context) => const LoginScreen(),
-        '/home': (context) => const HomeScreen(),
-        '/activity_tracking': (context) => const ActivityTrackingScreen(),
-        '/device_control': (context) => const DeviceControlScreen(),
-      },
+      theme: ThemeData(
+        primaryColor: const Color(0xFF8CCBD5),
+        scaffoldBackgroundColor: const Color(0xFF0A1128),
+        fontFamily: 'Arial',
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Color(0xFFFFFFFA)),
+          bodyMedium: TextStyle(color: Color(0xFFFFFFFA)),
+          titleLarge:
+              TextStyle(color: Color(0xFF8CCBD5), fontWeight: FontWeight.bold),
+          titleMedium:
+              TextStyle(color: Color(0xFFFFFFFA), fontWeight: FontWeight.w500),
+          labelMedium: TextStyle(color: Color(0xFFFFFFFA), fontSize: 14),
+        ),
+        colorScheme: ColorScheme.dark(
+          primary: const Color(0xFF8CCBD5),
+          secondary: const Color(0xFF3F797A),
+          background: const Color(0xFF0A1128),
+          surface: const Color(0xFF295B62).withOpacity(0.5),
+          onPrimary: const Color(0xFFFFFFFA),
+          onSecondary: const Color(0xFFFFFFFA),
+          onBackground: const Color(0xFFFFFFFA),
+          onSurface: const Color(0xFFFFFFFA),
+        ),
+      ),
+      home: const LoginScreen(),
     );
   }
 }
--- app-movil/aquiles/lib/screens/home_screen.dart
+++ app-movil/aquiles/lib/screens/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
-          'assets/logo.png',
+          'assets/images/logo.png',
           height: 40,
         ),
         actions: [
