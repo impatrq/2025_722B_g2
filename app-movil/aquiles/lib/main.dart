2581caff803dfd72c4e4a53092578551100cd395
Juan Montilla <Juanidequilmes@gmail.com>
2025-05-23 10:00:34 -0300

    feat: implement firebase

--- app-movil/aquiles/lib/main.dart
+++ app-movil/aquiles/lib/main.dart
@@ -4,12 +4,18 @@ import 'screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const AquilesApp());
 }
 
